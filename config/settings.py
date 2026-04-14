6b2f7f829966014c0c401dd1917bfd4b75fd8710
Denis <denis@example.com>
2026-04-14 14:20:48 +0000

    fix: database url parsing and static storage

--- config/settings.py
+++ config/settings.py
@@ -106,9 +106,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# Use PostgreSQL if DATABASE_URL is provided, otherwise fallback to SQLite (useful for PythonAnywhere free)
+# Use PostgreSQL if DATABASE_URL is provided, otherwise fallback to SQLite
+database_url = env('DATABASE_URL', default=f'sqlite:////{BASE_DIR}/db.sqlite3')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
 DATABASES = {
-    'default': env.db('DATABASE_URL', default=f'sqlite:////{BASE_DIR}/db.sqlite3')
+    'default': env.db_url_config(database_url)
 }
 
 
@@ -150,7 +154,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
